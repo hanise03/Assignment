51da7ad8cd6158f76c3da28b42083206c2e3e15e
hanise03 <hanisaqilah1003@gmail.com>
2025-11-01 22:43:55 +0800

    Change page title and visualisation for MotorBike

--- sidebar.py
+++ sidebar.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Student Survey"
+    page_title="MotorBike"
 )
 
-visualise = st.Page('studentSurvey.py', title='Pencapaian Akademik Pelajar', icon=":material/school:")
+visualise = st.Page('main.py', title='MotorBike', icon=":material/school:")
 
 home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
 
