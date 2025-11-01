5d1d0e9c21ce15f0c2a8b318a738aa39649b94a7
hanise03 <hanisaqilah1003@gmail.com>
2025-11-01 23:45:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -155,6 +155,8 @@ st.plotly_chart(fig3, use_container_width=True)
 # --- 2. STREAMLIT PAGE TITLE ---
 st.title("Biker Data Analysis")
 
+st.success("""The common objective for this group is to analyze how different factors (demographic, environmental, and behavioral) relate to each other, rather than focusing on the accident outcome.""")
+
 
 # --- 3. Visualization: Biker Age Distribution by Helmet Use ---
 st.header("Biker Age Distribution by Helmet Use")
