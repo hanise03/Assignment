3cdde8691c6dced6776c3df4018312a57d865b9b
hanise03 <hanisaqilah1003@gmail.com>
2025-11-02 15:47:20 +0800

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -4,10 +4,11 @@ st.set_page_config(
     page_title="MotorBike"
 )
 
-# You MUST ensure the following files exist in your directory:
-# 1. main.py
-# 2. accident_severity.py
-# 3. biker_data_analysis.py
+# You MUST ensure the following files exist in your directory with these EXACT names:
+# 1. home.py
+# 2. main.py
+# 3. Accident_Severity_Analysis.py
+# 4. Biker_Data_and_Environmental_exposure_Analysis.py
 
 visualise = st.Page('main.py', title='MotorBike', icon=":material/school:")
 severity_analysis = st.Page('Accident_Severity_Analysis.py', title='Accident Severity Analysis', icon=":material/bar_chart:")
