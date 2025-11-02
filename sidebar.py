efb8fdacdeabff11310193cbb8da8c7723b99e98
hanise03 <hanisaqilah1003@gmail.com>
2025-11-02 15:42:59 +0800

    Update sidebar.py

--- sidebar.py
+++ sidebar.py
@@ -4,8 +4,12 @@ st.set_page_config(
     page_title="MotorBike"
 )
 
-# New Pages added here:
-visualise = st.Page('main.py', title='Biker Accident Analysis', icon=":material/school:")
+# You MUST ensure the following files exist in your directory:
+# 1. main.py
+# 2. accident_severity.py
+# 3. biker_data_analysis.py
+
+visualise = st.Page('main.py', title='MotorBike', icon=":material/school:")
 severity_analysis = st.Page('accident_severity.py', title='Accident Severity Analysis', icon=":material/bar_chart:")
 biker_analysis = st.Page('biker_data_analysis.py', title='Biker Data and Environmental Exposure Analysis', icon=":material/group:")
 
@@ -13,7 +17,7 @@ home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:"
 
 pg = st.navigation(
     {
-        "Menu": [home, visualise, severity_analysis, biker_analysis] # Pages are added to the list here
+        "Menu": [home, visualise, severity_analysis, biker_analysis]
     }
 )
 
