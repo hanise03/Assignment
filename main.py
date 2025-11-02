2c32ff09eecd715d7cc8f6cff55f27347948aaf5
hanise03 <hanisaqilah1003@gmail.com>
2025-11-02 12:16:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import plotly.express as px
 import numpy as np
 import seaborn as sns
 
+col1, col2, col3, col4 = st.columns(4)
+   
+col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
+col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
+col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
+col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
 st.set_page_config(
     page_title="Analysis of Factors Influencing Motorbike Accident Severity"  # Changed page title
 )
