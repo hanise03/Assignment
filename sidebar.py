838cfed330a8974d67a4afc0333fc990b3e24a6a
hanise03 <hanisaqilah1003@gmail.com>
2025-11-02 15:33:47 +0800

    Add new pages for accident and biker analysis

--- sidebar.py
+++ sidebar.py
@@ -4,14 +4,17 @@ st.set_page_config(
     page_title="MotorBike"
 )
 
-visualise = st.Page('main.py', title='MotorBike', icon=":material/school:")
+# New Pages added here:
+visualise = st.Page('main.py', title='Biker Accident Analysis', icon=":material/school:")
+severity_analysis = st.Page('accident_severity.py', title='Accident Severity Analysis', icon=":material/bar_chart:")
+biker_analysis = st.Page('biker_data_analysis.py', title='Biker Data and Environmental Exposure Analysis', icon=":material/group:")
 
 home = st.Page('home.py', title='Homepage', default=True, icon=":material/home:")
 
 pg = st.navigation(
-        {
-            "Menu": [home, visualise]
-        }
-    )
+    {
+        "Menu": [home, visualise, severity_analysis, biker_analysis] # Pages are added to the list here
+    }
+)
 
 pg.run()
